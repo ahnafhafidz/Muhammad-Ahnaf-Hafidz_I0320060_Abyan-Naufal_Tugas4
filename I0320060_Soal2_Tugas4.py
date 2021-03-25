a06f14ae8c5112cb84098056c46edadb28d3c7bf
Ahnaf Hafidz <ahnaf.muhammad19@gmail.com>
2021-03-26 01:03:46 +0700

    Soal 2

---
+++ I0320060_Soal2_Tugas4.py
@@ -0,0 +1,11 @@
+#Soal 2
+
+#input angka pertama
+angka1 = int(input("Masukkan Angka Pertama: "))
+#input angka kedua
+angka2 = int(input("Masukkan Angka Kedua: "))
+
+#penggunaan operator floor
+hasil = angka1 // angka2
+
+print("Hasil: ", hasil)
