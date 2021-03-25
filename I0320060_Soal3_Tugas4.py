c685b01435f7cacbbc97341ebdd972f375f7a235
Ahnaf Hafidz <ahnaf.muhammad19@gmail.com>
2021-03-26 01:05:44 +0700

    Soal 3

---
+++ I0320060_Soal3_Tugas4.py
@@ -0,0 +1,11 @@
+#Soal 3
+
+#berat maksimum dalam kg
+beratmax = 50 * 0.45       #1 lbs = 0,45 kg
+print("Berat maksimal bagasi di konter check in: ", beratmax, "Kg")
+
+A = 110
+B = 49
+
+print("1. Apakah berat lebih dari 110 Kg diperbolehkan: ", A > beratmax)
+print("2. Apakah berat 49 Kg diperbolehkan: ", B > beratmax)
